521f8cb74c510a4f1c39fecd9cce3faf5e19626b
Ilya Novak <ilya.novak.dev@gmail.com>
2022-04-15 19:43:59 +0300

    refactor: Removed console log.

--- src/shared/lib/formatter.js
+++ src/shared/lib/formatter.js
@@ -2,6 +2,6 @@ export function formatDate (date) {
 	const year = date.getFullYear()
 	const month = date.getMonth() + 1
 	const day = date.getDate()
-	console.log(`${year}-${month < 10 ? "0" + month : month}-${day < 10 ? "0" + day : day}`)
+
 	return `${year}-${month < 10 ? "0" + month : month}-${day < 10 ? "0" + day : day}`
 }
